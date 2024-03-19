fecb07f44e8674269ec2f864a49094fd6f67e449
Willem Haring <101287746+willem-haring@users.noreply.github.com>
2024-03-19 13:32:09 +0100

    Delete lib/genericimport/importers directory

--- lib/genericimport/importers/utils.ts
+++
@@ -1,8 +0,0 @@
-import { ImportResource, ImportType } from "./iImportHandler.ts";
-
-
-export function test(obj: ImportResource, property: string, type: ImportType): boolean {
-  if (property in obj) return true;
-  throw new Error(`${type} Missing required attribute ${property} in obj ${JSON.stringify(obj)}`, { cause: "validation" });
-
-}
