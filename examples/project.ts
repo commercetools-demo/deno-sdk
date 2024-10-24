22427c67452aa76de24eab5f5b4586b788b9ac97
willem-haring <willem.haring@commercetools.com>
2024-10-24 16:14:50 +0200

    added a access to examples

--- examples/project.ts
+++ examples/project.ts
@@ -1,5 +1,15 @@
 import { sdk } from "@commercetoolsdemo/sdk"
 
+/**
+ * # project example
+ * 
+ * call with: 
+ * 
+ * ```bash
+ * deno -A jsr:@commercetoolsdemo/examples/project
+ * ```
+ */
+
 const handle = sdk.init()
 const project = await handle.root().get().execute().then(res=> res.body)
 console.log(project)
