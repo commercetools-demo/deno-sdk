0259d4c47a60a44b2b845e0c251ac947206c84a0
willem-haring <willem.haring@commercetools.com>
2024-10-24 16:02:28 +0200

    adding another example

--- examples/project.ts
+++ examples/project.ts
@@ -1,4 +1,4 @@
-import { sdk } from "jsr:@commercetoolsdemo/sdk"
+import { sdk } from "@commercetoolsdemo/sdk"
 
 const handle = sdk.init()
 const project = await handle.root().get().execute().then(res=> res.body)
